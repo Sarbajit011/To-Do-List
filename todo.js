7ea775661231619ac69889197de4c5dd28d489bd
Sarbajit011 <sarbajithalder029@gmail.com>
2024-06-28 20:38:14 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    var $list, $newItemForm;
+    $list = $('ul');
+    $newItemForm = $('#newItemForm');
+  
+    $newItemForm.on('submit', function(e) {
+      e.preventDefault();
+      var text = $('input:text').val();
+      $list.append('<li>' + text + '</li>');
+      $('input:text').val('');
+    });
+  
+    $list.on('click', 'li', function() {
+      var $this = $(this);
+      $this.remove();
+    });
+  
+  });
+  
